1f7774138f3f99da567e93cf522940f0b2d170d5
Raycia <raycialima@gmail.com>
2023-06-02 21:50:43 -0300

    realizando algumas alterações no código

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
-
 const url = 'https://api.github.com/users';
-const main = document.getElementById('main')
-// let user = 'marcelocant'
-let text = ''
+const main = document.getElementById('main');
+let text = '';
 
 function getUser(user) {
-
-    fetch(`${url}/${user}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
-        }
+  fetch(`${url}/${user}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      text += `<p>${data.name} possui ${data.public_repos} repositórios públicos no GitHub como ${data.login}</p>`;
+      main.innerHTML = text;
+      return Promise.all([data, fetch(data.avatar_url)]);
     })
-        .then((response) => response.json())
-        .then((data) => {
-            text += `<p>${data.name} possui ${data.public_repos} repositórios públicos no GitHub como ${data.login}</p>`
-            main.innerHTML = text
-        })
-        .catch((error) => console.error('Erro: ', error.message || error))
-
+    .then(([userData, avatarResponse]) => {
+      return avatarResponse.blob();
+    })
+    .then((avatarBlob) => {
+      const avatarURL = URL.createObjectURL(avatarBlob);
+      text += `<img src="${avatarURL}" alt="Foto do usuário">`;
+      main.innerHTML = text;
+    })
+    .catch((error) => console.error('Erro: ', error.message || error));
 }
 
-const userInput = document.getElementById('username')
+const userInput = document.getElementById('username');
 
-userInput.addEventListener('focusout', function(event){
-    getUser(event.target.value)
-})
-
-
+userInput.addEventListener('focusout', function (event) {
+  getUser(event.target.value);
+});
